46dc51e55dd3065acd7b607e02774ce5e56e4ebd
12philip34 <philipnssien@gmail.com>
2022-02-03 11:22:55 +0000

    first commit

---
+++ afric.js
@@ -0,0 +1,6 @@
+
+    function myFunction() {
+    document.getElementById("onclick").innerHTML = "by developing Mobile and Web applications that works, we help our clients deliver quality services to their existing and potential customers.\n" +
+        "\n" +
+        "            We have done it several times, we would sure to do it again!";
+}
